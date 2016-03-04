07403662aaedfc5d9783e394b88434f9328e79c7
Marco Flores <marcoflores@innology.mx>
2016-03-04 15:34:10 -0600

    Prueba Final

--- JuegoDeMemoria.playground/Contents.swift
+++ JuegoDeMemoria.playground/Contents.swift
@@ -1,23 +1,24 @@
 //: Playground - noun: a place where people can play
 
+
 import UIKit
 
 var numeros = 0...100
 
 for numero in numeros{
-    if (numero % 5) == 0{
-        print("\(numero) Bingo!!!")
+    if (numero%5) == 0{
+        print("# \(numero) Bingo!!!")
     }
     
     if (numero % 2) == 0{
-        print("\(numero) par!!!")
+        print("# \(numero) par!!!")
     }else{
-        print("\(numero) impar!!!")
+        print("# \(numero) impar!!!")
     }
     
     switch numero {
     case 30...40:
-        print("\(numero) Viva Swift!!!")
+        print("# \(numero) Viva Swift!!!")
         break
     default:
         break
